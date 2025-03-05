5e1bd4a458b6be99d7be06dfdfa34989f7053d4d
h581876 <581876@stud.hvl.no>
2025-03-05 16:49:16 +0100

    Metoder og test for tabell lagt til

--- src/Test/TabellMengdeTest.java
+++ src/Test/TabellMengdeTest.java
@@ -1,28 +1,133 @@
 package Test;
 
-import static org.junit.Assert.assertFalse;
-import static org.junit.Assert.assertTrue;
-
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import oblig3.mengde.MengdeADT;
 import oblig3.mengde.TabellMengde;
 
+import static org.junit.jupiter.api.Assertions.*;
+
+
 public class TabellMengdeTest {
-	
-	private TabellMengde<Integer> mengde1;
-	private TabellMengde<Integer> mengde2;
-	
-	@BeforeEach
-	public void setup() {
-		mengde1 = new TabellMengde<Integer>(10);
-		mengde2 = new TabellMengde<>(10);
-	}
-	
-	@Test
-	public void testErTom() {
-		
-		assertTrue(mengde1.erTom());
-		assertTrue(mengde2.erTom());
-	}
+
+    private TabellMengde<Integer> mengde1;
+    private TabellMengde<Integer> mengde2;
+
+    @BeforeEach
+    public void setup() {
+        mengde1 = new TabellMengde<>(10);
+        mengde2 = new TabellMengde<>(10);
+    }
+
+    @Test
+    public void testErTom() {
+        assertTrue(mengde1.erTom());
+        mengde1.leggTil(5);
+        assertFalse(mengde1.erTom());
+    }
+
+    @Test
+    public void testLeggTil() {
+        mengde1.leggTil(10);
+        assertTrue(mengde1.inneholder(10));
+        mengde1.leggTil(10); 
+        assertEquals(1, mengde1.antallElementer());
+    }
+
+    @Test
+    public void testFjern() {
+        mengde1.leggTil(20);
+        assertTrue(mengde1.inneholder(20));
+        Integer fjernetElement = mengde1.fjern(20);
+        assertEquals(20, fjernetElement);
+        assertFalse(mengde1.inneholder(20));
+    }
+
+    @Test
+    public void testUnion() {
+        mengde1.leggTil(1);
+        mengde1.leggTil(2);
+        mengde2.leggTil(2);
+        mengde2.leggTil(3);
+
+        MengdeADT<Integer> unionMengde = mengde1.union(mengde2);
+        assertTrue(unionMengde.inneholder(1));
+        assertTrue(unionMengde.inneholder(2));
+        assertTrue(unionMengde.inneholder(3));
+    }
+
+    @Test
+    public void testSnitt() {
+        mengde1.leggTil(5);
+        mengde1.leggTil(6);
+        mengde2.leggTil(6);
+        mengde2.leggTil(7);
+
+        MengdeADT<Integer> snittMengde = mengde1.snitt(mengde2);
+        assertTrue(snittMengde.inneholder(6));
+        assertFalse(snittMengde.inneholder(5));
+        assertFalse(snittMengde.inneholder(7));
+    }
+
+    @Test
+    public void testErLik() {
+        mengde1.leggTil(10);
+        mengde1.leggTil(20);
+        mengde2.leggTil(10);
+        mengde2.leggTil(20);
+
+        assertTrue(mengde1.erLik(mengde2));
+    }
+    
+    @Test
+    public void testMinus() {
+        mengde1.leggTil(10);
+        mengde1.leggTil(20);
+        mengde2.leggTil(20);
+        mengde2.leggTil(30);
+
+        MengdeADT<Integer> differanseMengde = mengde1.minus(mengde2);
+        assertTrue(differanseMengde.inneholder(10));
+        assertFalse(differanseMengde.inneholder(20));
+        assertFalse(differanseMengde.inneholder(30));
+    }
+    
+    @Test
+    public void testLeggTilAlleFra() {
+        mengde1.leggTil(10);
+        mengde1.leggTil(20);
+        mengde2.leggTil(20);
+        mengde2.leggTil(30);
+
+        mengde1.leggTilAlleFra(mengde2);
+        assertTrue(mengde1.inneholder(10));
+        assertTrue(mengde1.inneholder(20));
+        assertTrue(mengde1.inneholder(30));
+    }
+
+    @Test
+    public void testErDisjunkt() {
+        mengde1.leggTil(10);
+        mengde1.leggTil(20);
+        mengde2.leggTil(30);
+        mengde2.leggTil(40);
+
+        assertTrue(mengde1.erDisjunkt(mengde2));
+    }
+    
+    
+
+    @Test
+    public void testErDelmengdeAv() {
+        mengde1.leggTil(1);
+        mengde1.leggTil(2);
+        mengde1.leggTil(3);
+
+        mengde2.leggTil(1);
+        mengde2.leggTil(2);
+
+        assertTrue(mengde2.erDelmengdeAv(mengde1));
+    }
 }
+
--- src/oblig3/mengde/TabellMengde.java
+++ src/oblig3/mengde/TabellMengde.java
@@ -1,20 +1,22 @@
 package oblig3.mengde;
 
 public class TabellMengde<T> implements MengdeADT<T> {
-	
+
 	private T[] mengde;
 	private int antall;
-	
+
 	@SuppressWarnings("unchecked")
 	public TabellMengde(int kapasitet) {
-		mengde = (T[]) new Object [kapasitet];
+		mengde = (T[]) new Object[kapasitet];
 		antall = 0;
 	}
-	
+
+	@Override
 	public boolean erTom() {
 		return antall == 0;
 	}
-	
+
+	@Override
 	public boolean inneholder(T element) {
 		for (int i = 0; i < antall; i++) {
 			if (mengde[i].equals(element)) {
@@ -23,16 +25,136 @@ public class TabellMengde<T> implements MengdeADT<T> {
 		}
 		return false;
 	}
-	
+
+	@Override
 	public boolean erDelmengdeAv(MengdeADT<T> annenMengde) {
 		TabellMengde<T> annen = (TabellMengde<T>) annenMengde;
-		
+
 		for (int i = 0; i < antall; i++) {
 			if (annen.inneholder(mengde[i])) {
 				return true;
 			}
 		}
 		return false;
+	}
+
+	@Override
+	public boolean erLik(MengdeADT<T> annenMengde) {
+		TabellMengde<T> annen = (TabellMengde<T>) annenMengde;
+		if (antall != annen.antall) {
+			return false;
+		}
+		for (int i = 0; i < antall; i++) {
+			if (!annen.inneholder(mengde[i])) {
+				return false;
+			}
+		}
+		return true;
+	}
+
+	@Override
+	public boolean erDisjunkt(MengdeADT<T> annenMengde) {
+		TabellMengde<T> annen = (TabellMengde<T>) annenMengde;
+
+		for (int i = 0; i < antall; i++) {
+			if (annen.inneholder(mengde[i])) {
+				return false;
+			}
+		}
+		return true;
+	}
+
+	@Override
+	public void leggTil(T element) {
+		if (!inneholder(element)) {
+			if (antall == mengde.length) {
+				T[] nyMengde = (T[]) new Object[mengde.length * 2];
+				
+				for (int i = 0; i < antall; i++) {
+					nyMengde[i] = mengde[i];
+				}
+				mengde = nyMengde; 
+			}
+			mengde[antall] = element; 
+			antall++;
+		}
+	}
+
+	@Override
+	public MengdeADT<T> snitt(MengdeADT<T> annenMengde) {
+		TabellMengde<T> snittMengde = new TabellMengde<>(mengde.length);
+
+		for (int i = 0; i < antall; i++) {
+			if (annenMengde.inneholder(mengde[i])) {
+				snittMengde.leggTil(mengde[i]);
+			}
+		}
+		return snittMengde;
+	}
+
+	@Override
+	public int antallElementer() {
+		return antall;
+	}
+
+	@SuppressWarnings("unchecked")
+	@Override
+	public T[] tilTabell() {
+		T[] resultat = (T[]) new Object[antall];
+		for (int i = 0; i < antall; i++) {
+			resultat[i] = mengde[i];
+		}
+		return resultat;
+	}
+
+	@Override
+	public MengdeADT<T> union(MengdeADT<T> annenMengde) {
+		TabellMengde<T> unionMengde = new TabellMengde<>(mengde.length + annenMengde.antallElementer());
+
+		for (int i = 0; i < antall; i++) {
+			unionMengde.leggTil(mengde[i]);
+		}
+		for (int i = 0; i < annenMengde.antallElementer(); i++) {
+			T element = annenMengde.tilTabell()[i];
+			unionMengde.leggTil(element);
+		}
+		return unionMengde;
+	}
+
+	@Override
+	public MengdeADT<T> minus(MengdeADT<T> annenMengde) {
+		TabellMengde<T> differanseMengde = new TabellMengde<>(mengde.length);
+
+		for (int i = 0; i < antall; i++) {
+			if (!annenMengde.inneholder(mengde[i])) {
+				differanseMengde.leggTil(mengde[i]);
+			}
+		}
+		return differanseMengde;
+	}
+	
+	@Override
+	public void leggTilAlleFra(MengdeADT<T> annenMengde) {
+		for (int i = 0; i < annenMengde.antallElementer(); i++) {
+			T element = (T) annenMengde.tilTabell()[i];
+			leggTil(element);
+		}
+	}
+	
+	@Override
+	public T fjern(T element) {
+		for (int i = 0; i < antall; i++) {
+			if (mengde[i].equals(element)) {
+				T fjernElement = mengde[i];
+				for (int j = i; j < antall - 1; j++) {
+					mengde[j] = mengde[j+1];
+				}
+				mengde[antall - 1] = null;
+				antall--;
+				return fjernElement;
+			}
+		}
+		return null;
 	}
 
 }
